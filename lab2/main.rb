2dfc7c982fe1c3c36f5f65718d3aa36015aa0712
Emil <emil2002.04.02@gmail.com>
2021-09-11 12:19:17 +0300

    fix:inp_word add:ui

--- lab2/main.rb
+++ lab2/main.rb
@@ -1,10 +1,11 @@
-def word(num, word)
+def inp_word
+    puts "Введите слово: "
+    word = gets.chomp
     size_word = word.length
-    (word.to_s.slice(size_word-2, size_word-1) == "cs") ? puts("#{2**size_word}") : puts("#{word.reverse}")
-
+    puts (word.slice(size_word-2, size_word) == "cs") ? ("#{2**size_word}") : ("#{word.reverse}")
 end
 
-def arr_pokemons
+def add_pokemons
     print "Введите количество покемонов "
     i = gets.to_i
     pokemons = []
@@ -18,5 +19,25 @@ def arr_pokemons
     puts pokemons
 end
 
-#word 2, "kics"
-arr_pokemons
+input = nil
+
+loop do
+    
+    puts "Выберите пункт меню"
+    puts "1 - inp_word"
+    puts "2 - add_pokemons"
+    puts "-1 - quit"
+    input = gets.to_i
+    puts "Вы ввели #{input}"
+
+    case input
+    when 1
+        inp_word
+    when 2
+        add_pokemons
+    when -1
+        break
+    else
+        puts "Вы ввели неверный символ"
+    end
+end
