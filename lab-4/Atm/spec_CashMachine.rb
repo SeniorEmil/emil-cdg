ebe76b14ee8ced7087f8cc39f78895e198eba095
Emil-Ibraimov <92825360+Emil-Ibraimov@users.noreply.github.com>
2021-12-10 23:47:25 +0300

    rename

---
+++ lab-4/Atm/spec_CashMachine.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+require 'stringio'
+
+require './CashMachine.rb'
+
+RSpec.describe CashMachine do
+    before { @atm = CashMachine.new}
+
+    #ввод депозита
+    it 'deposit entry' do
+        allow_any_instance_of(Kernel).to receive(:gets).and_return("D", "200", "Q")
+        expect(@atm.init)
+        expect(File.read("balance.txt")).to eql("300.0")
+    end
+
+    #вывод средств
+    it 'withdrawal of funds' do
+        allow_any_instance_of(Kernel).to receive(:gets).and_return("W", "200", "Q")
+        expect(@atm.init)
+        expect(File.read("balance.txt")).to eql("100.0")
+    end
+
+    #показать баланс
+    it 'balance' do
+      allow_any_instance_of(Kernel).to receive(:gets).and_return("B", "Q")
+      expect(@atm.init)
+      expect(File.read("balance.txt")).to eql("100.0")
+    end
+
+end
